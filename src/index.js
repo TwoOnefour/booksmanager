9921e652c5a6737feec3b2703f54f079feeda306
twoonefour <lys214412@gmail.com>
2024-12-07 19:18:38 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+  async fetch(request, env) {
+    const { pathname, searchParams } = new URL(request.url);
+    
+    if (pathname === "/api/select_all") {
+      const { results } = await env.DB.prepare(
+        "SELECT name,author,publisher,publish_date FROM books",
+      )
+        .all();
+      return Response.json(results);
+    }
+    if (pathname === "/api/select_book_by_index"
+       && searchParams.has("index")
+      ){
+      const { results } = await env.DB.prepare(
+        "SELECT * FROM books LIMIT ?, 1",
+      )
+        .bind(searchParams.get("index"))
+        .all();
+      return Response.json(results);
+    }
+    return new Response(
+      "404 not found.",
+    );
+  },
+}
